4617b1773a60af6a3db228c24a64ffb619eaa83e
will mcbrien <williamjamesmcbrien@gmail.com>
2018-11-02 16:56:42 +0000

    Add more tests for airtraffic controller

--- spec/air_traffic_controller_spec.rb
+++ spec/air_traffic_controller_spec.rb
@@ -10,7 +10,12 @@ describe AirTrafficController do
   end
 
   it 'can instruct a plane to land at an airport' do
+    allow(weather).to receive(:stormy?).and_return(false)
+    allow(airport1).to receive(:full?).and_return(false)
+    allow(airport1).to receive(:allow_landing).and_return(true)
     expect(subject).to respond_to(:ask_plane_to_land)
+    expect(subject.ask_plane_to_land(aeroplane1, airport1, weather)).to eq(true)
+
   end
 
   it 'can instuct a plane to take off from and airport' do
